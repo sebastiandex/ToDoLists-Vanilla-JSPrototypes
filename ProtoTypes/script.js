69f4b82733937e98c60b27e0d4de497e3f70755a
Andrew <sebastiandex@gmail.com>
2021-08-25 22:12:44 +1000

    Добавлено удаление таски

--- ProtoTypes/script.js
+++ ProtoTypes/script.js
@@ -2,8 +2,8 @@ const input = document.getElementById('textValue');
 const taskContainer = document.getElementById("taskContainer");
 const defaultDiv = function (id, label) {
     const defDiv = document.createElement('div');
-    defDiv.innerHTML = label;
-    defDiv.style.width = '70%';
+    defDiv.innerHTML = `${label} <button type="submit" onclick="this.parentElement.remove()" style='background-color: red; color: white; cursor: pointer'>Done!</button>`;
+    defDiv.style.width = '70%'
     defDiv.style.textAlign = 'center'
     defDiv.style.backgroundColor = 'white'
     defDiv.style.border = '1px black solid'
@@ -17,16 +17,16 @@ const defaultDiv = function (id, label) {
         defDiv
     )
 };
+const taskArray = [];
 const addNewTask = () => {
-    let newDiv = new defaultDiv(2, input.value);
-    console.log(defaultDiv())
-    console.log(newDiv)
-    taskContainer.appendChild(newDiv);
-    document.getElementById('textValue').value = '';
-
+    if (input.value !== '' ) {
+        let newDiv = new defaultDiv(taskArray.length + 1, input.value);
+        taskContainer.appendChild(newDiv);
+        taskArray.push(taskArray.length + 1)
+        document.getElementById('textValue').value = '';
+    }
 };
 const enterSender = (e) => {
-    console.log(e)
     if (e.keyCode === 13) {
         addNewTask();
     }
